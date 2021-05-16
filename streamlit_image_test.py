edf0b582c2cdbd23191801e7a58e6256b4643f5d
Madhu Milan <madhumilan37@gmail.com>
2021-05-16 10:00:38 +0200

    Adding model.h5

--- streamlit_image_test.py
+++ streamlit_image_test.py
@@ -12,7 +12,7 @@ dataset = os.path.join(path,"cats-and-dogs/")
 print(dataset)
 '''
 path = os.getcwd()
-loaded_model = tf.keras.models.load_model(os.path.join(path,'cats-and-dogs'))
+loaded_model = tf.keras.models.load_model(os.path.join(path,'model.h5'))
 
 def file_selector(folder_path='.'):
     filenames = os.listdir(folder_path)
